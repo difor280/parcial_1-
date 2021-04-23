f181b4b38c8af27c0a2bfb0da8dce4f16f53243f
unknown <80016811+difor280@users.noreply.github.com>
2021-04-23 13:42:03 -0300

    funcion imagen completa

--- tinkercad_parcial_11.ino
+++ tinkercad_parcial_11.ino
@@ -34,12 +34,13 @@ void loop()
       case 1:       
        verificacion();
        Serial.println("todos los leds funcion correctamente \n porfavor presione cualquier tecla para volver al menu principal \n \n \n");
-       while(Serial.available()==0){}
-  		break;
-  	  case 2:
-     	 break;
-      case 3:
+      while(Serial.available()==0){}
        break;
+  	  case 2:
+       imagen();
+       break;
+      case 3:
+      break;
       default:
       Serial.println("el dato ingresado no se contempla en el menu \n \n \n");
     }
@@ -59,18 +60,52 @@ void inicializacion()
    	digitalWrite(reloj_salida,0);
 }
 
-void apagar()
-{ // apagar los leds despues de una ejecucion 
-  for(int i=1; i <=64; i++)
-  {
-    digitalWrite (SER,0);
-    
-    inicializacion();
-  }
-  delay (10000);
+
+  
+
+void patron(int guardado[][8], int num_arre)
+{	
+   	int num,tamano=0, cifra, aux;
+  	Serial.print("ingrese el numero que representan los leds de la fila: ");
+  	Serial.print(num_arre);
+    Serial.print("\n"); 
+    while(Serial.available()==0){}
+  	num=Serial.parseInt();
+    while(num < 0)
+    {Serial.print("ingrese el numero que representan los leds prendidos \n");
+    while(Serial.available()==0){}
+  	num=Serial.parseInt();
+    }
+  	aux= num;
+  	while(num>0)
+    {
+      cifra= num % 10;
+      if(cifra !=9 and cifra !=0)
+      {
+        tamano++;
+      }
+      num=num/10;
+    }
+  int espacios[tamano],ingresor[8]={0,0,0,0,0,0,0,0};
+
+  	ingreso(espacios,aux,cifra);
+  	orden(espacios,tamano,ingresor);
+  	guardador(guardado,ingresor,num_arre);
 }
 
-
+void ingreso(int espacios[],int aux,int cifra )
+{	int e=0;  
+  	while(aux>0)
+    {
+      cifra = aux % 10;
+      if (cifra !=9 and cifra !=0)
+      {
+        espacios[e]=cifra;
+      	e++;
+      }
+      aux= aux / 10;
+    }  
+}
 
 void grupo8(int cadena[])
 {	//recorrer las filas dejando datos, pide una cadena de enteros 
@@ -81,14 +116,21 @@ void grupo8(int cadena[])
     }
 }  
 
-int orden(int espacios[],int cambios)
+void orden(int espacios[],int tamano,int fin[])
+{ 
+  int a;
+	for(int i=0; i<tamano ;i++){
+      a=espacios[i]-1;
+      fin[a]=1;
+    }  
+}
+
+void guardador(int arreglodarreglos[][8],int ingreso[],int num_arre)
 {
-  int ordenar[8]={0,0,0,0,0,0,0,0};
-	for(int i=0;i<cambios;i++)
+	for(int i= 0 ; i <8;i++)
     {
-      ordenar[espacios[i]]=1;
-    }
-  return *ordenar;
+      arreglodarreglos[num_arre][i]=*(ingreso+i);
+    }	
 }
 
 void verificacion()
@@ -96,23 +138,126 @@ void verificacion()
   for(int i=1;i<=64;i++)
   {
     digitalWrite(SER,1);
-    
     inicializacion();  	 
   } 
 }
 
 
-/*void imagen()
-{
+void imagen()
+{	int patronn[8][8];
+  Serial.print("Menu de la funcion imagen\n");
+  Serial.print(" presiona 1. para ingresar tu propio patron\n presiona 2. Para los patrones predefinidos y seleccionar alguno de ellos \n \n");
+  while(Serial.available()==0){} 
+  opciones=Serial.parseInt();
   
+  switch(opciones)
+  {
+    case 1:
+    	///Serial.print("ingrese el numero que representan los leds prendidos \n ");
+    	//Serial.print("Ejemplo: si usted ingresa 1468, estara pidiendo que se enciendan los leds en la posicion 1,4,6,8");
+    	
+    	for(int i=0;i<8;i++)
+        {
+          patron(patronn,i);
+          grupo8(patronn[i]);
+        }
+    	
+    	
+    break;
+    case 2:
+    Serial.print("Los patrones predefinidos son las letras del alfabeto mayusculo \n ingresa un numero como como te muestro a continuacion :");
+    Serial.print("1 --> A \n 2 --> B \n 3 --> C \n 4 --> D \n 5 --> E \n 6 --> F \n 7 --> G \n 8 --> H \n 9 --> I \n 10 --> J \n 1 --> K \n 12 --> L \n 13 --> M \n 14 --> N \n 15 --> O \n 16 --> P \n 17 --> Q \n 18 --> R \n 19 --> S \n 20 --> T \n 21 --> U \n 22 --> V \n 23 --> W \n 24 --> X \n 25 --> Y \n 26 --> Z \n");
+    while(Serial.available()==0){} 
+    opciones=Serial.parseInt();  
+    switch (opciones)
+    {
+      case 1:
+      letra_A();
+      break;}
+      /*case 2:
+      letra_B();
+      break;
+      case 3:
+      letra_C();
+      break;
+      case 4:
+      letra_D();
+      break;
+      case 5:
+      letra_E();
+      break;
+      case 6:
+      letra_F();
+      break;
+      case 7:
+      letra_G();
+      break;
+      case 8:
+      letra_H();
+      break;
+      case 9:
+      letra_I();
+      break;
+      case 10:
+      letra_J();
+      break;
+      case 11:
+      letra_K();
+      break;
+      case 12:
+      letra_L();
+      break;
+      case 13:
+      letra_M();
+      break;
+      case 14:
+      letra_N();
+      break;
+      case 15:
+      letra_O();
+      break;
+      case 16:
+      letra_P();
+      break;
+      case 17:
+      letra_Q();
+      break; 
+      case 18:
+      letra_R();
+      break;
+      case 19:
+      letra_S();
+      break;
+      case 20:
+      letra_T();
+      break;
+      case 21:
+      letra_U();
+      break;
+      case 22:
+      letra_V();
+      break;
+      case 23:
+      letra_W();
+      break;
+      case 24:
+      letra_X();
+      break;
+      case 25:
+      letra_Y();
+      break;
+      case 26:
+      letra_Z();
+      break;
+    }*/
+    break;
+  }
 }
- */
+ 
 void publik()
 {
   int tiempo=0;
   int num_patrones=0;
-  
-  
   Serial.println("Ingrese la cantidad de patrones que desea: ");
   while(Serial.available()==0){}
   num_patrones = Serial.parseInt();
@@ -136,12 +281,13 @@ void publik()
     tiempo = Serial.parseInt();
   }
   
-  
   /*for(int i=0;i <= num_patrones; i++)
-    { 
-    	imagen();
-    }*/
+  {
+    
+  }*/
 }
+
+
 
 //catalogo de funciones prestablezidas 
 void letra_A()
@@ -158,30 +304,25 @@ void letra_A()
   	for(int i =0;i<8;i++ )
     {
     	grupo8(todo[i]);
-    } 
-	
+    }
 }
 
-void letra_B()
+/*void letra_B()
 {
     int todo[8][8]={
       	 {1,1,1,1,1,1,1,0}
 		,{1,1,0,0,0,0,1,1}
 		,{1,1,0,0,0,0,1,1}
-		,{1,1,1,1,1,1,1,0}
+        ,{1,1,1,1,1,1,1,0}
 		,{1,1,1,1,1,1,1,1}
 		,{1,1,0,0,0,0,1,1}
 		,{1,1,0,0,0,0,1,1}
 		,{1,1,1,1,1,1,1,0}
 };
-  	
   	for(int i =0;i<8;i++ )
     {
     	grupo8(todo[i]);
     } 
-    
-  	
-  
 }
 
 void letra_C()
@@ -200,7 +341,6 @@ void letra_C()
   {
     grupo8(todo[i]);
   }
- 
 }
 
 void letra_D()
@@ -217,6 +357,368 @@ void letra_D()
   for(int i=0;i<8;i++)
   {
     grupo8(todo[i]);
-  }
- 
+  } 
 }
+void letra_E()
+{
+  int todo[8][8]={
+    {1,1,1,1,1,1,1,1},
+	{1,1,1,1,1,1,1,1},
+	{1,1,0,0,0,0,0,0},
+	{1,1,1,1,1,1,0,0},
+	{1,1,1,1,1,1,0,0},
+	{1,1,0,0,0,0,0,0},
+	{1,1,1,1,1,1,1,1},
+	{1,1,1,1,1,1,1,1} };
+  for(int i=0;i<8;i++)
+  {
+    grupo8(todo[i]);
+  } 
+}
+void letra_F()
+{
+  int todo[8][8]={
+	{1,1,1,1,1,1,1,1},
+	{1,1,1,1,1,1,1,1},
+	{1,1,0,0,0,0,0,0},
+	{1,1,1,1,1,1,0,0},
+	{1,1,1,1,1,1,0,0},
+	{1,1,0,0,0,0,0,0},
+	{1,1,0,0,0,0,0,0},
+	{1,1,0,0,0,0,0,0} };
+  for(int i=0;i<8;i++)
+  {
+    grupo8(todo[i]);
+  } 
+}
+void letra_G()
+{
+  int todo[8][8]={
+	{1,1,1,1,1,1,1,1},
+	{1,1,1,1,1,1,1,1},
+	{1,1,0,0,0,0,0,0},
+	{1,1,0,1,1,1,1,1},
+	{1,1,0,1,1,1,1,1},
+	{1,1,0,0,0,0,1,1},
+	{1,1,1,1,1,1,1,1},
+	{1,1,1,1,1,1,1,1} };
+  for(int i=0;i<8;i++)
+  {
+    grupo8(todo[i]);
+  } 
+}
+void letra_H()
+{
+  int todo[8][8]={
+	{1,1,0,0,0,0,1,1},
+	{1,1,0,0,0,0,1,1},
+	{1,1,0,0,0,0,1,1},
+	{1,1,1,1,1,1,1,1},
+	{1,1,1,1,1,1,1,1},
+	{1,1,0,0,0,0,1,1},
+	{1,1,0,0,0,0,1,1},
+	{1,1,0,0,0,0,1,1} };
+  for(int i=0;i<8;i++)
+  {
+    grupo8(todo[i]);
+  } 
+}
+void letra_I()
+{
+  int todo[8][8]={
+	{1,1,1,1,1,1,1,1},
+	{1,1,1,1,1,1,1,1},
+	{0,0,0,1,1,0,0,0},
+	{0,0,0,1,1,0,0,0},
+	{0,0,0,1,1,0,0,0},
+	{0,0,0,1,1,0,0,0},
+	{1,1,1,1,1,1,1,1},
+	{1,1,1,1,1,1,1,1} };
+  for(int i=0;i<8;i++)
+  {
+    grupo8(todo[i]);
+  } 
+}
+
+void letra_J()
+{
+  int todo[8][8]={
+	{1,1,1,1,1,1,1,1},
+	{1,1,1,1,1,1,1,1},
+	{0,0,0,1,1,1,0,0},
+	{0,0,0,1,1,1,0,0},
+	{0,0,0,1,1,1,0,0},
+	{1,1,1,1,1,1,0,0},
+	{1,1,1,1,1,1,0,0},
+	{1,1,1,1,1,1,0,0} };
+  for(int i=0;i<8;i++)
+  {
+    grupo8(todo[i]);
+  } 
+}
+void letra_K()
+{
+  int todo[8][8]={
+	{1,1,0,0,0,1,1,1},
+	{1,1,0,0,1,1,1,0},
+	{1,1,0,1,1,1,0,0},
+	{1,1,1,1,1,0,0,0},
+	{1,1,1,1,1,0,0,0},
+	{1,1,0,1,1,1,0,0},
+	{1,1,0,0,1,1,1,0},
+	{1,1,0,0,0,1,1,1} };
+  for(int i=0;i<8;i++)
+  {
+    grupo8(todo[i]);
+  } 
+}
+void letra_L()
+{
+  int todo[8][8]={
+	{1,1,0,0,0,0,0,0},
+	{1,1,0,0,0,0,0,0},
+	{1,1,0,0,0,0,0,0},
+	{1,1,0,0,0,0,0,0},
+	{1,1,0,0,0,0,0,0},
+	{1,1,1,1,1,1,1,1},
+	{1,1,1,1,1,1,1,1},
+	{1,1,1,1,1,1,1,1} };
+  for(int i=0;i<8;i++)
+  {
+    grupo8(todo[i]);
+  } 
+}
+void letra_M()
+{
+  int todo[8][8]={
+	{1,1,0,0,0,0,1,1},
+	{1,1,1,1,0,0,1,1},
+	{1,1,0,1,1,0,1,1},
+	{1,1,0,0,1,1,1,1},
+	{1,1,0,0,0,1,1,1},
+	{1,1,0,0,0,0,1,1},
+	{1,1,0,0,0,0,1,1},
+	{1,1,0,0,0,0,1,1} };
+  for(int i=0;i<8;i++)
+  {
+    grupo8(todo[i]);
+  } 
+}
+void letra_N()
+{
+  int todo[8][8]={
+	{1,1,0,0,0,0,1,1},
+	{1,1,1,0,0,0,1,1},
+	{1,1,1,1,0,0,1,1},
+	{1,1,0,1,1,0,1,1},
+	{1,1,0,0,1,1,1,1},
+	{1,1,0,0,0,1,1,1},
+	{1,1,0,0,0,0,1,1},
+	{1,1,0,0,0,0,1,1} };
+  for(int i=0;i<8;i++)
+  {
+    grupo8(todo[i]);
+  } 
+}
+void letra_O()
+{
+  int todo[8][8]={
+	{0,1,1,1,1,1,1,0},
+	{1,1,1,1,1,1,1,1},
+	{1,1,1,0,0,1,1,1},
+	{1,1,0,0,0,0,1,1},
+	{1,1,0,0,0,0,1,1},
+	{1,1,1,0,0,1,1,1},
+	{1,1,1,1,1,1,1,1},
+	{0,1,1,1,1,1,1,0} };
+  for(int i=0;i<8;i++)
+  {
+    grupo8(todo[i]);
+  } 
+}
+void letra_P()
+{
+  int todo[8][8]={
+	{1,1,1,1,1,1,1,0},
+	{1,1,1,1,1,1,1,1},
+	{1,1,0,0,0,0,1,1},
+	{1,1,0,0,0,0,1,1},
+	{1,1,1,1,1,1,1,1},
+	{1,1,1,1,1,1,1,0},
+	{1,1,0,0,0,0,0,0},
+	{1,1,0,0,0,0,0,0} };
+  for(int i=0;i<8;i++)
+  {
+    grupo8(todo[i]);
+  } 
+}
+void letra_Q()
+{
+  int todo[8][8]={
+	{0,1,1,1,1,1,1,0},
+	{1,1,1,1,1,1,1,0},
+	{1,1,0,0,0,1,1,0},
+	{1,1,0,1,0,1,1,0},
+	{1,1,0,0,1,1,1,0},
+	{1,1,0,0,0,1,1,0},
+	{1,1,1,1,1,1,1,0},
+	{0,1,1,1,1,1,0,1} };
+  for(int i=0;i<8;i++)
+  {
+    grupo8(todo[i]);
+  } 
+  
+}
+void letra_R()
+{
+  int todo[8][8]={
+	{1,1,1,1,1,1,1,0},
+	{1,1,1,1,1,1,1,0},
+	{1,1,0,0,0,0,1,1},
+	{1,1,0,0,0,0,1,1},
+	{1,1,1,1,1,1,1,1},
+	{1,1,1,1,1,1,1,0},
+	{1,1,0,0,1,1,0,0},
+	{1,1,0,0,0,1,1,0} 
+	};
+  for(int i=0;i<8;i++)
+  {
+    grupo8(todo[i]);
+  }  
+}
+
+void letra_S()
+{
+  int todo[8][8]={
+	{1,1,1,1,1,1,1,1},
+	{1,1,1,1,1,1,1,1},
+	{1,1,0,0,0,0,0,0},
+	{1,1,1,1,1,1,1,1},
+	{1,1,1,1,1,1,1,1},
+	{0,0,0,0,0,0,1,1},
+	{1,1,1,1,1,1,1,1},
+	{1,1,1,1,1,1,1,1} };
+  for(int i=0;i<8;i++)
+  {
+    grupo8(todo[i]);
+  }  
+}
+void letra_T()
+{
+  int todo[8][8]={
+	{1,1,1,1,1,1,1,1},
+	{1,1,1,1,1,1,1,1},
+	{1,1,1,1,1,1,1,1},
+	{0,0,1,1,1,1,0,0},
+	{0,0,1,1,1,1,0,0},
+	{0,0,1,1,0,1,0,0},
+	{0,0,1,1,1,1,0,0},
+	{0,0,1,1,1,1,0,0} };
+  for(int i=0;i<8;i++)
+  {
+    grupo8(todo[i]);
+  }  
+}
+void letra_U()
+{
+  int todo[8][8]={
+	{1,1,1,0,0,1,1,1},
+	{1,1,1,0,0,1,1,1},
+	{1,1,1,0,0,1,1,1},
+	{1,1,1,0,0,1,1,1},
+	{1,1,1,0,0,1,1,1},
+	{1,1,1,1,1,1,1,1},
+	{1,1,1,1,1,1,1,1},
+	{0,1,1,1,1,1,1,0}};
+  for(int i=0;i<8;i++)
+  {
+    grupo8(todo[i]);
+  }  
+}
+void letra_W()
+{
+  int todo[8][8]={
+	{1,1,0,0,0,0,1,1}
+	,{1,1,0,0,0,0,1,1}
+	,{1,1,0,0,0,0,1,1}
+	,{1,1,0,0,0,0,1,1}
+	,{1,1,0,1,1,0,1,1}
+	,{1,1,0,1,1,0,1,1}
+	,{1,1,0,1,1,0,1,1}
+	,{0,1,1,0,0,1,1,0}};
+  for(int i=0;i<8;i++)
+  {
+    grupo8(todo[i]);
+  }  
+}
+void letra_V()
+{
+  int todo[8][8]={
+	{1,1,0,0,0,0,1,0}
+	,{1,1,0,0,0,0,1,0}
+	,{0,1,1,0,0,0,1,0}
+	,{0,1,1,0,0,1,1,0}
+	,{0,0,1,0,0,1,0,0}
+	,{0,0,1,1,1,1,0,0}
+	,{0,0,1,1,1,1,0,0}
+	,{0,0,0,1,1,0,0,0}};
+  for(int i=0;i<8;i++)
+  {
+    grupo8(todo[i]);
+  }  
+}
+void letra_X()
+{
+  int todo[8][8]={
+
+ {1,1,0,0,0,0,1,1}
+,{0,1,1,0,0,1,1,0}
+,{0,0,1,1,1,1,0,0}
+,{0,0,0,1,1,0,0,0}
+,{0,0,1,1,1,1,0,0}
+,{0,1,1,0,0,1,1,0}
+,{0,1,1,0,0,1,1,0}
+,{1,1,0,0,0,0,1,1}};
+  for(int i=0;i<8;i++)
+  {
+    grupo8(todo[i]);
+  }  
+}
+void letra_Y()
+{
+  int todo[8][8]={
+
+{1,1,0,0,0,0,1,1},
+{0,1,1,0,0,1,1,0},
+{0,1,1,0,0,1,1,0},
+{0,0,1,1,1,1,0,0},
+{0,0,0,1,1,0,0,0},
+{0,0,0,1,1,0,0,0},
+{0,0,0,1,1,0,0,0},
+{0,0,0,1,1,0,0,0}};
+  for(int i=0;i<8;i++)
+  {
+    grupo8(todo[i]);
+  }  
+}
+void letra_Z()
+{
+  int todo[8][8]={
+{1,1,1,1,1,1,1,1},
+{1,1,1,1,1,1,1,1},
+{0,0,0,0,1,1,0,0},
+{0,0,0,1,1,0,0,0},
+{0,0,1,1,0,0,0,0},
+{0,1,1,0,0,0,0,0},
+{1,1,1,1,1,1,1,1},
+{1,1,1,1,1,1,1,1}};
+  for(int i=0;i<8;i++){grupo8(todo[i]);}  
+}
+*/
+
+
+
+
+
+
+
